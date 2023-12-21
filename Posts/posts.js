5fc85a7a4ad59018612ad5cd4fd2cc1d448355f0
Efreed-Shadwin <146854297+Efreed-Shadwin@users.noreply.github.com>
2023-12-21 09:03:22 -0600

    Added simple post JS

--- Posts/posts.js
+++ Posts/posts.js
@@ -1,3 +1,21 @@
 /* Posts Page JavaScript */
 
 "use strict";
+const apiBaseURL = "https://microbloglite.onrender.com";
+// Backup server:   https://microbloglite.herokuapp.com
+function posts() {
+    fetch(HOST + "/api/posts?limit=100&offset=0", {
+        method: "GET",
+        headers: { 'Authorization': `Bearer ${localStorage.token}` }
+    })
+    .then(response => response.json())
+    .then(output=>{
+        console.log(JSON.stringify(output,0,4));
+        document.body.innerHTML += output.map(
+            p=> `<div class="card ">
+                    <h5>${p.username} (${p.createdAt})</h5>  
+                    <h6>"${p.text} [${p.likes}]:</h6>"
+                </div>`
+        ).join("");
+    });
+}
