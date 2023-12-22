e6cfe9ff113c3cd5a6720040d2c3bc91c3cb91ab
TrevorBelmont <tbelmont97@gmail.com>
2023-12-22 13:29:21 -0600

    Microblog 2023

---
+++ page.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const apiBaseURL = "https://microbloglite.onrender.com";
+    const SIGNUP = document.querySelector("input[type=submit]")
+    
+});
