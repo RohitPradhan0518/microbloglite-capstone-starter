a35ad8e58c7907c0714780cc8b54383f72667a1e
Efreed-Shadwin <146854297+Efreed-Shadwin@users.noreply.github.com>
2024-01-03 13:41:28 -0600

    Corrected post aiming at the wrong page

--- Posts/posts.js
+++ Posts/posts.js
@@ -13,13 +13,13 @@ function renderPosts(data){
 }
 document.addEventListener("DOMContentLoaded",e=>{
     const token=localStorage.token;
-    fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts?limit=1000&offset=0",{
+    fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts?limit=25&offset=0",{
         headers:{"Authorization" : `Bearer ${token}`}                    
     })
     .then(response=>response.json())
     .then(renderPosts)    
 })
-async function submitPost(data={}){
+async function createPost(data={}){
     try{
         const response =await fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts", {
             method: "POST" ,
@@ -32,4 +32,36 @@ async function submitPost(data={}){
         console.error("Error:",error);
     }
 }
-document.addEventListener("onclick"(),)
+document.getElementById("submitPost").onclick=createPost(document.getElementById("message"))
+function logout () {
+    const loginData = getLoginData();
+
+    // GET /auth/logout
+    const options = { 
+        method: "GET",
+        headers: { 
+            // This header is how we authenticate our user with the
+            // server for any API requests which require the user
+            // to be logged-in in order to have access.
+            // In the API docs, these endpoints display a lock icon.
+            Authorization: `Bearer ${loginData.token}`,
+        },
+    };
+
+    fetch(apiBaseURL + "/auth/logout", options)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .finally(() => {
+            // We're using `finally()` so that we will continue with the
+            // browser side of logging out (below) even if there is an 
+            // error with the fetch request above.
+
+            window.localStorage.removeItem("login-data");  // remove login data from LocalStorage
+            window.location.assign("/");  // redirect back to landing page
+        });
+}
+document.getElementById("logout").onclick=logout()
+function getLoginData () {
+    const loginJSON = window.localStorage.getItem("login-data");
+    return JSON.parse(loginJSON) || {};
+}
